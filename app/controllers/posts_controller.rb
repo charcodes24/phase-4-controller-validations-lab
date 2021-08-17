3b1c23fbfd1b7416d53bf3dfd71918b76a73dc75
charcodes24 <charcodes24@gmail.com>
2021-08-17 12:46:10 -0700

    commit tree d23655c91dcf60c31bc6ca86d22078e5a402665e

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,15 @@
-class PostsController < ApplicationController
+class PostsController < ApplicationController 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
-    post = Post.find(params[:id])
+    post = find_post
     
     render json: post
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post = find_post
+    post.update!(post_params)
     render json: post
   end
 
@@ -18,6 +17,14 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def find_post
+    Post.find(params[:id])
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
